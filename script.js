1e98f1dd57d72b5228d3f532d477ec71b292e0e8
David <YI.LI85@myhunter.cuny.edu>
2025-04-01 17:01:10 -0400

    updated comments

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function scrollToSection(sectionId) {
     }
 }
 
-let themeButton = document.getElementById("theme-button");
+let themeButton = document.getElementById("theme-button"); //themeButton = theme-button
 let navbar = document.querySelector(".navigation-bar"); // navbar = navigation-bar
-let button = document.querySelector(".button"); // navbar = navigation-bar
+let button = document.querySelector(".button"); // button = button
 
 const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
   navbar.classList.toggle("dark-mode"); // Toggle dark mode on navbar as well
-  button.classList.toggle("dark-mode");
+  button.classList.toggle("dark-mode"); // Toggle dark mode on button as well
   localStorage.setItem('dark-mode', document.body.classList.contains("dark-mode"));
 };
 
@@ -23,12 +23,12 @@ const toggleDarkMode = () => {
 if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-mode');
     navbar.classList.add('dark-mode'); // Apply dark mode to navbar if it's enabled
-    button.classList.toggle("dark-mode");
+    button.classList.toggle("dark-mode"); // Apply dark mode to button if it's enabled
 }
 
 themeButton.addEventListener("click", toggleDarkMode);
 
-const socialIcons = document.querySelectorAll(".social-icon");
+const socialIcons = document.querySelectorAll(".social-icon"); // socialIcons = social-icon
 
 // Apply filter to icons when dark mode is enabled
 if (document.body.classList.contains("dark-mode")) {
